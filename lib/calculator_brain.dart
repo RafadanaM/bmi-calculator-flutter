dc75841effa1b6bb3e02cf98e993b5588a19abb3
Rafadana Mountheira <rafadanam@gmail.com>
2020-07-29 03:27:45 +0700

    add calculateBMI method

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -1,6 +1,17 @@
-class CalculatorBrain {
-  final int height;
-  final int weight;
+import 'dart:math';
 
-  CalculatorBrain(this.height, this.weight);
+class CalculatorBrain {
+  final int _height;
+  final int _weight;
+
+  double _bmi;
+
+  CalculatorBrain(this._height, this._weight);
+
+  String calculateBMI() {
+    _bmi = _weight / pow(_height / 100, 2);
+    return _bmi.toStringAsFixed(1);
+  }
+
+
 }
